3a35b66914a6d77e6ec81b04a88e93844e765ee8
secona <77039267+secona@users.noreply.github.com>
2025-05-13 17:43:38 +0700

    feat: initial subscriber code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(clippy::empty_loop)]
+
+use borsh_derive::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, HandleError, MessageHandler, QueueProperties};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String,
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        println!("In 2306152411's computer. Message received: {:?}", message);
+
+        Ok(())
+    }
+
+    fn get_handler_action(&self) -> String {
+        String::from("user_created")
+    }
+}
+
+fn main() -> ! {
+    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_string()).unwrap();
+
+    let _ = listener.listen(String::from("user_created"), UserCreatedHandler, QueueProperties {
+        auto_delete: false,
+        durable: false,
+        use_dead_letter: true,
+    });
+
+    loop {}
 }
